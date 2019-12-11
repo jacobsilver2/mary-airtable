df3e6c6ddfc329cd49671532803b4b92e76ebf3f
Jacob Silver <jacobsilver2@mac.com>
2019-12-11 13:04:22 -0500

    removed auth0 entirely

--- src/pages/canadian-museum-for-human-rights/index.js
+++ src/pages/canadian-museum-for-human-rights/index.js
@@ -2,20 +2,15 @@ import axios from "axios"
 import React, { useEffect, useState } from "react"
 import WorkContainerComponent from "../../components/workContainer"
 import { cmhr } from "../../utility/airtableUrls"
-import { isAuthenticated, login } from "../../utility/auth"
 import { renderWorkHtml } from "../../utility/renderHtml"
 
-const CMHRPage = props => {
+const CMHRPage = () => {
   const [nodes, setNodes] = useState([])
   useEffect(() => {
     axios.get(cmhr).then(response => {
       setNodes(response.data.records)
     })
   }, [])
-  if (!isAuthenticated()) {
-    login()
-    return <p>Redirecting to login...</p>
-  }
 
   const hero = nodes.filter(node => node.fields.Type === "Hero")[0]
   const myhtml = nodes

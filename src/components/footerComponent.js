c7ada9827734920330434b8a1b663519f6dca1a3
Jacob Silver <jacobsilver2@mac.com>
2020-02-03 20:18:00 -0500

    basic addition of projects footer

--- src/components/footerComponent.js
+++ src/components/footerComponent.js
@@ -1,24 +1,60 @@
 import React from "react"
-import { navigate, Link } from "gatsby"
-import { StyledHTwo } from "../styles/StyledHtml"
-import Scroll from 'react-scroll';
+import { useStaticQuery, graphql, Link } from "gatsby"
+import styled, { createGlobalStyle } from "styled-components"
+import Img from "gatsby-image"
 
-const FooterComponent = () => {
-  function goBack() {
-    // navigate("/")
-    Scroll.scrollToTop();
-  }
+const FooterContainer = styled.footer`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+`
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    {
+      allAirtable(
+        filter: { table: { eq: "WorkHome" } }
+        sort: { fields: data___Order }
+      ) {
+        nodes {
+          data {
+            Name
+            Order
+            passwordProtected
+            Attachments {
+              localFiles {
+                childImageSharp {
+                  fluid {
+                    ...GatsbyImageSharpFluid
+                  }
+                }
+              }
+            }
+          }
+          id
+        }
+      }
+    }
+  `)
+  const { nodes } = data.allAirtable
+  console.log(nodes)
+  const randoms = data.allAirtable.nodes
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 4)
+    .map(selection => (
+      <Link to={`/${selection.data.Name}`} key={selection.id}>
+        <Img
+          fluid={selection.data.Attachments.localFiles[0].childImageSharp.fluid}
+        />
+      </Link>
+    ))
 
   return (
-    <div>Footer</div>
-    // <StyledHTwo
-    //   style={{ textAlign: "center", cursor: "pointer" }}
-    //   onClick={goBack}
-    // >
-    //   back to top
-    // </StyledHTwo>
-    // <Link to="/#top">back to top</Link>
+    <>
+      <h2 style={{ marginTop: "15rem" }}>More Work</h2>
+      <FooterContainer>{randoms}</FooterContainer>
+      <h2 style={{ textAlign: "center" }}>
+        <Link to="/">Back To Top</Link>
+      </h2>
+    </>
   )
 }
-
-export default FooterComponent

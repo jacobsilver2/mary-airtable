4fc561679970b0cf9402032871e9592be9aed10b
Jacob Silver <jacobsilver2@mac.com>
2020-02-05 08:38:39 -0500

    totally rocked the footer

--- src/components/footerComponent.js
+++ src/components/footerComponent.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import styled from "styled-components"
 import Img from "gatsby-image"
 import useWindowWidth from "../hooks/useWindowWidth"
+import GridItem from "../components/gridItem"
+import { TextWrapper, StyledHTwo } from "../styles/StyledHtml"
 
 const FooterContainer = styled.footer`
   display: grid;
@@ -12,7 +14,7 @@ const FooterContainer = styled.footer`
   @media only screen and (min-width: 540px) and (max-width: 767px) {
     grid-template-columns: 1fr 1fr;
   }
-  @media only screen and (max-width: 766px) {
+  @media only screen and (max-width: 539px) {
     grid-template-columns: 1fr;
   }
 `
@@ -44,10 +46,11 @@ export default ({ location }) => {
       }
     }
   `)
-
+  const { nodes } = data.allAirtable
+  const [randomTiles, setRandomTiles] = useState([])
   const width = useWindowWidth()
-  let numberOfTiles = null
 
+  let numberOfTiles = null
   if (width >= 768) {
     numberOfTiles = 4
   } else if (width < 768 && width >= 540) {
@@ -56,24 +59,41 @@ export default ({ location }) => {
     numberOfTiles = 1
   }
 
-  const { nodes } = data.allAirtable
-  //get a random selection of nodes, exluding the node you're currently at.
-  const randoms = nodes
-    .filter(node => node.data.Name !== location)
-    .sort(() => 0.5 - Math.random())
-    .slice(0, numberOfTiles)
-    .map(selection => (
-      <Link to={`/${selection.data.Name}`} key={selection.id}>
-        <Img
-          fluid={selection.data.Attachments.localFiles[0].childImageSharp.fluid}
-        />
-      </Link>
-    ))
+  //get a random selection of nodes, exluding the node you're currently at. The random number of tiles will change when it hits different screen
+  // resolutions.
+
+  useEffect(() => {
+    const randoms = nodes
+      .filter(node => node.data.Name !== location)
+      .sort(() => 0.5 - Math.random())
+      .slice(0, numberOfTiles)
+      .map(selection => (
+        <Link
+          key={selection.id}
+          to={
+            selection.data.passwordProtected
+              ? `/${selection.data.Name}/`
+              : `${selection.data.Name}`
+          }
+        >
+          <GridItem
+            isProtected={selection.data.passwordProtected}
+            title={selection.data.Name}
+            fluid={
+              selection.data.Attachments.localFiles[0].childImageSharp.fluid
+            }
+          />
+        </Link>
+      ))
+    setRandomTiles(randoms)
+  }, [numberOfTiles])
 
   return (
     <>
-      <h2 style={{ marginTop: "15rem" }}>More Work</h2>
-      <FooterContainer>{randoms}</FooterContainer>
+      <TextWrapper>
+        <StyledHTwo style={{ marginTop: "15rem" }}>More Work</StyledHTwo>
+      </TextWrapper>
+      <FooterContainer>{randomTiles}</FooterContainer>
       <h2 style={{ textAlign: "center" }}>
         <Link to="/">Back To Top</Link>
       </h2>
